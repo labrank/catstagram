90d813a1f9a4ec77c11396aba318dafce158a007
Juan J. Cuautle <labrank@gmail.com>
2019-02-17 18:48:23 -0600

    changed serve

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ util.batchCats().then(() =>{
   console.log("download finished");
 });
 
-app.use(serveStatic(__dirname + "/dist"));
+
 app.get("/cats", async(req, res, next) =>  {
   var cats = await db.buscar()
        .then(function(rows){
@@ -21,7 +21,7 @@ app.get("/cats", async(req, res, next) =>  {
            console.error);
  res.json(cats);
 });
-
+app.use(serveStatic(__dirname + "/dist"));
 app.listen(port, () => {
  console.log("Server running on port 3000");
 });
